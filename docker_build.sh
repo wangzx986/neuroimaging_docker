b7798bff32a05a15f942f0f117a33e15a1ae7ecb
wangzx <35664099+wangzx986@users.noreply.github.com>
2024-09-17 17:56:00 -0700

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -4,5 +4,5 @@ docker image build \
     --build-arg USERNAME=${USER} \
     --build-arg USER_ID=$(id -u ${USER}) \
     --build-arg GROUP_ID=$(id -g ${USER}) \
-    -t wangzx94/neuroimaging_py311_update:latest \
+    -t image_name:latest \
     -f neuroimaging_py311_update.Dockerfile .
